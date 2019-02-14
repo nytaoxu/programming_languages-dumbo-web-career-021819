57e3ba106136c340f04adab4ca0c8ad2b73fae62
Tao Xu <nytaoxu@gmail.com>
2019-02-14 19:20:39 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,8 +2,11 @@ def reformat_languages(languages)
   # your code here
   result = Hash.new
   languages.each do |style, lang_hash|
+    match = false
     language_hash.each do |lang_sym, type_hash|
-      result.each do |r_lang_sym, 
+      result.each do |r_lang_sym, r_attributes|
+        
+      end
     end
   end
   result
